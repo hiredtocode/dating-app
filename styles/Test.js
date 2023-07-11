f55de590a4a7b4821dc858785813e83053e46ac7
hiredtocode <jasonhan@kakao.com>
2023-07-11 22:04:41 +0900

    Styled component test

---
+++ styles/Test.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+export const Text = styled.Text`
+  font-size: 18px;
+  color: #0606f5;
+  font-weight: 500;
+`;
