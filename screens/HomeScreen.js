1e6ec96e07c433af60b0ef3c8e31c487abb75d57
hiredtocode <jasonhan@kakao.com>
2023-07-11 21:58:23 +0900

    Button to navigate to Chat screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+    
   return (
     <View>
       <Text>I am the HomeScreen</Text>
+      <Button title='Go to Chat Screen' onPress={() => navigation.navigate('Chat')} />
     </View>
   )
 }
