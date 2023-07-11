5af825091969946e399a15d21bce8844eb2286b6
hiredtocode <jasonhan@kakao.com>
2023-07-11 21:52:47 +0900

    Navigation stack

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>I am the HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
